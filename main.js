aac20ae2e901a4dddd172749a0982b2a74822111
Salt <luffy.w.pirate@gmail.com>
2020-09-28 23:34:14 +0800

    List rendering

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const app = Vue.createApp({
       product: 'Socks',
       image: './assets/images/socks_green.jpg',
       inventory: 100,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ],
     };
   },
 });
