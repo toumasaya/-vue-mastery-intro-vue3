ce65de3bb21410319d69752ba4bb263a02f29515
Salt <luffy.w.pirate@gmail.com>
2020-09-28 23:09:49 +0800

    Creating the Vue app

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Green and blue socks here...',
+    };
+  },
+});
+
+const mountApp = app.mount('#app');
